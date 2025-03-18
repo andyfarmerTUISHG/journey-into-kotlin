ad7a7f350e0bca4c1145c098265c2c23efbbb335
Andy Farmer <andy.farmer@travelopia.com>
2025-03-18 15:08:59 +0000

    More Loops including Named Loops

--- src/Main.kt
+++ src/Main.kt
@@ -1,89 +1,123 @@
 package src
 
-fun main(){
+fun main() {
 	terminalSplit("Loops")
-	loopDetails()
+//	loopDetails()
+//	conditionalLoop()
+	labeledLoop()
 }
-fun loopDetails(){
-println("for loop")
-	for(i in 1..10){
+
+fun conditionalLoop() {
+	var loopNumber = 0
+	while (loopNumber < 10) {
+		loopNumber++ //prevent infinite loop
+		if (loopNumber == 5) {
+			continue // this will skip to the next iteration
+//			break // this will break the loop
+		}
+		println("Looping $loopNumber")
+
+	}
+}
+
+fun labeledLoop(){
+	var number = 0
+//	label your outer loop
+	outerLoop@ while (number < 5) {
+		number++
+		println(number)
+		var i = 0
+		while ( i < 5) {
+			if (i == 3 && number == 3) break@outerLoop //@outerLoop
+			i++
+			println("Inner loop $i")
+		}
+	}
+}
+fun loopDetails() {
+	println("for loop")
+	for (i in 1..10) {
 		println("I am looping i = $i")
 	}
 	println("until loop")
-	for (i in 1 until 10){
+	for (i in 1 until 10) {
 		println("I am looping in an until i = $i")
 	}
 	println("downto loopcount backwards")
-	for(i in 10 downTo 1){
+	for (i in 10 downTo 1) {
 		println("I am looping downwards i = $i")
 	}
 	println("step loop")
-	for ( i in 1 until 12 step 2) {
+	for (i in 1 until 12 step 2) {
 		println("I am looping i = $i")
 	}
 	println("While Loop")
 	var number = 0
-	while( number < 10){
+	while (number < 10) {
 		println(number++)
 	}
 	println("Do While Loop")
 	var maxNumber = 8
 	do {
 		println("Need this at least once $maxNumber")
-	} while ( maxNumber++ < 10)
+	} while (maxNumber++ < 10)
 }
-fun mainFunctions(){
-	val max = getMax(5,9)
+
+fun mainFunctions() {
+	val max = getMax(5, 9)
 	println("Max value is $max")
-	val maxDouble = getMax(5.5,9.9)
+	val maxDouble = getMax(5.5, 9.9)
 	println("MaxDouble value is $maxDouble")
-	val maxTripleInt = getMax(9,6,17)
+	val maxTripleInt = getMax(9, 6, 17)
 	println("maxTripleInt value is $maxTripleInt")
 
 	sendMessage("Alexa", "Hello")
 	sendMessage("Alexa")
 	sendMessage()
 	sendMessage(message = "Named message")
-	sendMessage(message = "Named message", name="Siri")
+	sendMessage(message = "Named message", name = "Siri")
 
-	println(sum(1,3,5,7))
+	println(sum(1, 3, 5, 7))
 	println("The sum function is restricted to 4 parameters, if the number of th parameters is unkown you can use vararg")
-	println(sumVarArg(1,3,5,7,9,11))
+	println(sumVarArg(1, 3, 5, 7, 9, 11))
 }
 
-fun sumVarArg(vararg numbers: Int):Int{
+fun sumVarArg(vararg numbers: Int): Int {
 	var sum = 0
-	for (number in numbers){
+	for (number in numbers) {
 		sum += number
 	}
 	return sum
 }
 
-fun sum(a: Int, b: Int, c: Int, d: Int):Int {
+fun sum(a: Int, b: Int, c: Int, d: Int): Int {
 	return a + b + c + d
 }
 
-fun sendMessage(name: String ="User", message: String = sendText()){
+fun sendMessage(name: String = "User", message: String = sendText()) {
 	println("$name and message =  $message")
 }
 
 fun sendText() = "Some default text from a function"
 
-fun getMax(num1: Int, num2: Int):Int{
-	return if(num1 > num2) num1 else num2
+fun getMax(num1: Int, num2: Int): Int {
+	return if (num1 > num2) num1 else num2
 }
-fun getMax(num1: Int, num2: Int, num3: Int):Int{
-	if(num1 >= num2 && num1 >= num3){
+
+fun getMax(num1: Int, num2: Int, num3: Int): Int {
+	if (num1 >= num2 && num1 >= num3) {
 		return num1
-	} else if (num2 >= num1 && num2 >= num3){
+	} else if (num2 >= num1 && num2 >= num3) {
 		return num2
 	} else {
 		return num3
 	}
 }
-fun getMax(num1: Double, num2: Double):Double{
-	return if(num1 > num2) num1 else num2
+
+fun getMax(num1: Double, num2: Double): Double {
+	return if (num1 > num2) num1 else num2
 }
+
 fun mainDataTypes() {
 	println("Welcome to Main - Learning")
 	println("Introducing Variables")
@@ -124,7 +158,7 @@ fun mainDataTypes() {
 	nullExample()
 }
 
-fun nullExample(){
+fun nullExample() {
 	var text: String? = null
 	text = "Name"
 //	println(text.length) // you cant use length on non null objects
@@ -136,9 +170,10 @@ fun nullExample(){
 
 	println(text2)
 }
-fun controlFlow(){
+
+fun controlFlow() {
 	val alarm = 7
-	when (alarm){
+	when (alarm) {
 		12, 7, 14 -> println("Wake Up - the time is $alarm")
 //		7 -> println("Wake Up - the time is $alarm")
 //		14 -> println("Wake Up - the time is $alarm")
@@ -147,7 +182,7 @@ fun controlFlow(){
 	}
 }
 
-fun combinedOperator(){
+fun combinedOperator() {
 	val isPlaying = true
 	val score = 80
 	if (isPlaying && score == 100) {
@@ -165,7 +200,7 @@ fun combinedOperator(){
 	}
 }
 
-fun ifelseDetails(){
+fun ifelseDetails() {
 	val isActive = false
 	if (isActive == true) {
 		println("User is Active")
@@ -178,7 +213,7 @@ fun ifelseDetails(){
 	// Currently Greater than >
 	if (grade > 90) {
 		println("You got a Distinction")
-	} else  if (grade >= 70)	{
+	} else if (grade >= 70) {
 		println("You got a Merit")
 	} else if (grade >= 50) {
 		println("You got a pass")
@@ -190,20 +225,22 @@ fun ifelseDetails(){
 	val result = if (grade >= 50) "Pass" else "Fail"
 	println("Your Result is $result")
 }
-fun commentsDetails(){
+
+fun commentsDetails() {
 	println("Not going to see much here!!!")
 	// End of line comment
 	/*
 		Multi line comment
 	 */
 }
-fun operatorsDetails(){
+
+fun operatorsDetails() {
 	var x = 5
 	val y = 3.0
 	//Operand are x & y
 	val result = x + y
 	println("Result = $result")
-	println("As an Expression in a literal\nx + y = ${x +y}")
+	println("As an Expression in a literal\nx + y = ${x + y}")
 	println("x - y = ${x - y}")
 	println("x * y = ${x * y}")
 	println("x / y = ${x / y}")
@@ -240,7 +277,7 @@ fun operatorsDetails(){
 
 }
 
-fun numberOptions(){
+fun numberOptions() {
 	var number: Int = 22
 
 	val maxIntegerVal: Int = Int.MAX_VALUE
@@ -282,7 +319,7 @@ fun numberOptions(){
 
 }
 
-fun floatOptions(){
+fun floatOptions() {
 	println("Used when needing more precision in your numbers")
 
 	val myNumber = 3.141
@@ -294,14 +331,14 @@ fun floatOptions(){
 
 }
 
-fun charOptions(){
+fun charOptions() {
 	val myCharValue: Char = 'D'
 	val myBooleanValue: Boolean = true
 	println("My Char Value (can only hold one character) = $myCharValue")
 	println("My Boolean Value = $myBooleanValue")
 }
 
-fun dataTypesOptions(){
+fun dataTypesOptions() {
 	println("8 Data Types")
 	println("Byte")
 	println("Short")
@@ -313,6 +350,6 @@ fun dataTypesOptions(){
 	println("Boolean")
 }
 
-fun terminalSplit(terminalTitle: String ){
+fun terminalSplit(terminalTitle: String) {
 	println("\n---------\n$terminalTitle\n")
 }

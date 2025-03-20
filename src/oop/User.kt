f077813128c892e16eafe697bfe738bc2d387fa6
Andy Farmer <andy.farmer@travelopia.com>
2025-03-20 10:30:14 +0000

    User Object - Define default paramaters in the class to negate secondary constructors

--- src/oop/User.kt
+++ src/oop/User.kt
@@ -1,28 +1,29 @@
 package src.oop
 
+// You can specify default values in the original constructor
 class User(
-	name: String,
-	var lastname: String,
-	var age: Int
+	firstname: String,
+	var lastname: String = "LastName",
+	var age: Int = 0
 ) {
 	// Only allow names with that start with an A
 	/// need an Initialisation block
 	var name: String
 
 	init {
-	    if(name.lowercase().startsWith("a") || name.lowercase().startsWith("j")) {
-			this.name = name
+	    if(firstname.lowercase().startsWith("a") || firstname.lowercase().startsWith("j")) {
+			this.name = firstname
 		} else {
 			this.name = "User"
-			println("The name (${name}) does not start with the Letter 'a' or 'A' or 'j' or 'J'")
+			println("The firstname (${firstname}) does not start with the Letter 'a' or 'A' or 'j' or 'J'")
 		}
 	}
 	// you can have multiple init blocks
 
 	//Secondary Constructors - you can have multiple
-	constructor(name:String) : this(name, "User-Lastname", 0)
-	constructor(name: String, lastname: String) : this(name, lastname, 0){
-		println("3rd constructor used")
-	}
+//	constructor(firstname:String) : this(firstname, "User-Lastname", 0)
+//	constructor(name: String, lastname: String) : this(name, lastname, 0){
+//		println("3rd constructor used")
+//	}
 
 }

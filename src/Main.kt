b58368ff89fa9ac71ec51bdeb7941824e8c2e1ee
Andy Farmer <andy.farmer@travelopia.com>
2025-03-25 17:16:08 +0000

    Inheritance - refactor

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,15 @@
 package src
 
 import src.challenge.BankAccount
-import src.oop.Calculator
-import src.oop.Car
-import src.oop.Automobile
-import src.oop.Plane
-import src.oop.User
+import src.oop.*
 import src.oop.examples as oop
 import src.learning.examples as learning
 
 
 fun main() {
 	terminalSplit("Inheritance")
-	val ford = Automobile(model = "Ford", name = "Fiesta", colour = "Candy Blue", engines = 1, doors = 5, wheels = 4)
+	Inheritance().hello()
 
-	println("Name = ${ford.name}")
-	ford.move()
-	ford.stop()
-
-	val cesna = Plane(model = "Cessna", name = "Cessna 172", colour = "White", doors = 2,engines = 2,  wings = 2)
-	cesna.move()
-	cesna.stop()
 }
 
 
--- src/oop/Inheritance.kt
+++ src/oop/Inheritance.kt
@@ -1,6 +1,17 @@
 package src.oop
 
 class Inheritance {
+	open fun hello(){
+		val ford = Automobile(model = "Ford", name = "Fiesta", colour = "Candy Blue", engines = 1, doors = 5, wheels = 4)
+
+		println("Name = ${ford.name}")
+		ford.move()
+		ford.stop()
+
+		val cesna = Plane(model = "Cessna", name = "Cessna 172", colour = "White", doors = 2,engines = 2,  wings = 2)
+		cesna.move()
+		cesna.stop()
+	}
 }
 
 open class Vehicle(val name: String, val model: String, val colour: String, val engines: Int, val doors: Int) {

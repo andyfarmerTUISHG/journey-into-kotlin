37a7fe3e64bb66daa1e82a6a44077664c05e3b95
Andy Farmer <andy.farmer@travelopia.com>
2025-03-26 14:52:00 +0000

    Object Expression Expression Example

--- src/oop/ObjectExpression.kt
+++ src/oop/ObjectExpression.kt
@@ -3,5 +3,33 @@ package src.oop
 class ObjectExpression {
 	fun hello() {
 		println("Object Expression")
+
+
+		// User an anonymous function / object expression -
+		// keeps the code clean and adhere to inheritance
+		val loginButton = Button("Login", 1, object : OnClickListener {
+			override fun onClick() {
+				println("Login Button Clicked")
+			}
+		})
+		val signupButton = Button("Signup", 2, object : OnClickListener {
+			override fun onClick() {
+				println("Signup Button Clicked")
+			}
+		})
+	}
+}
+
+interface OnClickListener {
+	fun onClick()
+}
+
+class Button(val text: String, val id: Int, onClickListener: OnClickListener) {
+
+}
+
+class ClickListener() : OnClickListener {
+	override fun onClick() {
+		println("Button Clicked")
 	}
 }
